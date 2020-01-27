18b40266f7605f60b8307c37015457d76f7ddaf4
Musab Agab <musabagab2010@gmail.com>
2020-01-27 10:04:20 +0300

    added custom path cliper

--- lib/ui/views/landing_view.dart
+++ lib/ui/views/landing_view.dart
@@ -3,8 +3,27 @@ import 'package:flutter/material.dart';
 class LandingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return ClipPath(
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: Image.network(
+            'https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=387&q=80'),
+      ),
+      clipper: MyClipper(),
     );
   }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(size.height, 0.0);
+    path.lineTo(0.0, size.width);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
