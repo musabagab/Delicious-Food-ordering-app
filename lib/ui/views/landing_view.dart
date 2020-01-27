d1bee46d64abde8c3e86d4978e1c06c7d9e74b89
Musab Agab <musabagab2010@gmail.com>
2020-01-27 11:21:54 +0300

    added the custom path to the image

--- lib/ui/views/landing_view.dart
+++ lib/ui/views/landing_view.dart
@@ -1,23 +1,27 @@
 import 'package:delicious/ui/widgets/background.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class LandingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Background();
+    return SafeArea(
+      child: Stack(
+        children: <Widget>[
+          Background(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ClipPath(
+              clipper: RoundedDiagonalPathClipper(),
+              child: Container(
+                width: double.infinity,
+                height: 500,
+                color: Colors.white.withOpacity(.7),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
-}
-
-class MyClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    path.lineTo(size.height, 0.0);
-    path.lineTo(0.0, size.width);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
