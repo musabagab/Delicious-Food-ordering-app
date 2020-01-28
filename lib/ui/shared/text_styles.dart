e3c20964beab74f998f6d077f43c94b72848b99b
Musab Agab <musabagab2010@gmail.com>
2020-01-28 14:26:15 +0300

    form style is now done

--- lib/ui/shared/text_styles.dart
+++ lib/ui/shared/text_styles.dart
@@ -4,3 +4,8 @@ const headerStyle =
     TextStyle(fontSize: 35, fontWeight: FontWeight.w900, color: Colors.white);
 const subHeaderStyle =
     TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500, color: Colors.white);
+const labelStyle = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+    backgroundColor: Color(0xFFF56F0A));
--- lib/ui/widgets/authentication_form.dart
+++ lib/ui/widgets/authentication_form.dart
@@ -4,51 +4,128 @@ import 'package:delicious/ui/shared/text_styles.dart';
 import 'package:delicious/ui/views/base_view.dart';
 import 'package:flutter/material.dart';
 
-class AuthenticationForm extends StatelessWidget {
+class AuthenticationForm extends StatefulWidget {
+  @override
+  _AuthenticationFormState createState() => _AuthenticationFormState();
+}
+
+class _AuthenticationFormState extends State<AuthenticationForm> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return BaseView<AuthenticationModel>(
         builder: (context, model, child) => Container(
-              color: primaryColor,
+              color: Colors.white,
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      FlatButton(
-                        onPressed: () {
-                          model.changeSelectedForm();
-                        },
-                        child: Text(
-                          'Login',
-                          style: subHeaderStyle,
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          color: model.selectedForm == 'Login'
+                              ? Colors.white
+                              : primaryColor,
+                          child: InkWell(
+                            onTap: () {
+                              model.changeSelectedForm();
+                            },
+                            child: Center(
+                              child: Text(
+                                'Login',
+                                style: model.selectedForm == 'Login'
+                                    ? TextStyle(
+                                        fontSize: 25,
+                                        color: primaryColor,
+                                        fontWeight: FontWeight.w600)
+                                    : TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.w600),
+                              ),
+                            ),
+                          ),
                         ),
                       ),
-                      FlatButton(
-                        onPressed: () {
-                          model.changeSelectedForm();
-                        },
-                        child: Text(
-                          'Register',
-                          style: subHeaderStyle,
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          color: model.selectedForm == 'Register'
+                              ? Colors.white
+                              : primaryColor,
+                          child: InkWell(
+                            onTap: () {
+                              model.changeSelectedForm();
+                            },
+                            child: Center(
+                              child: Text(
+                                'Register',
+                                style: model.selectedForm == 'Register'
+                                    ? TextStyle(
+                                        fontSize: 25,
+                                        color: primaryColor,
+                                        fontWeight: FontWeight.w600)
+                                    : TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.w600),
+                              ),
+                            ),
+                          ),
                         ),
                       )
                     ],
                   ),
-                  model.selectedForm == 'Login'
-                      ? Container(
-                          child: Text(
-                            'LoginForm',
-                            style: headerStyle,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: model.selectedForm == 'Login'
+                        ? Form(
+                            key: _formKey,
+                            child: Column(
+                              children: <Widget>[
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                    hintText: 'Email ID',
+                                    icon: Icon(
+                                      Icons.email,
+                                      color: primaryColor,
+                                    ),
+                                  ),
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return 'Please enter some text';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                    hintText: 'Password',
+                                    icon: Icon(
+                                      Icons.lock,
+                                      color: primaryColor,
+                                    ),
+                                  ),
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return 'Please enter some text';
+                                    }
+                                    return null;
+                                  },
+                                )
+                              ],
+                            ),
+                          )
+                        : Container(
+                            child: Text(
+                              'Register Form',
+                              style: headerStyle,
+                            ),
                           ),
-                        )
-                      : Container(
-                          child: Text(
-                            'Register Form',
-                            style: headerStyle,
-                          ),
-                        )
+                  )
                 ],
               ),
             ));
