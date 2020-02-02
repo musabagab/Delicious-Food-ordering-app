b7b512f7291cb1415c5b8c352fb139647f5ddc30
Musab Agab <musabagab2010@gmail.com>
2020-02-02 08:41:16 +0300

    added ensurevisibile widget

--- lib/ui/widgets/forms/apptextform.dart
+++ lib/ui/widgets/forms/apptextform.dart
@@ -7,16 +7,19 @@ class AppTextForm extends StatelessWidget {
       this.textController,
       this.hintText,
       this.iconData,
-      this.errorMessage});
+      this.errorMessage,
+      this.focusNode});
 
   final TextEditingController textController;
   final String hintText;
   final IconData iconData;
   final String errorMessage;
+  final FocusNode focusNode;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      focusNode: focusNode,
       decoration: buildInputDecoration(hintText: hintText, iconData: iconData),
       controller: textController,
       validator: (value) => validateField(value, errorMessage),
--- lib/ui/widgets/forms/register_form.dart
+++ lib/ui/widgets/forms/register_form.dart
@@ -4,6 +4,7 @@ import 'package:delicious/ui/shared/app_colors.dart';
 import 'package:delicious/ui/shared/ui_helpers.dart';
 import 'package:delicious/ui/views/base_view.dart';
 import 'package:delicious/ui/widgets/buttons/busy_button.dart';
+import 'package:delicious/ui/widgets/ensure_visible.dart';
 import 'package:delicious/ui/widgets/forms/apptextform.dart';
 import 'package:flutter/material.dart';
 
@@ -17,10 +18,15 @@ class _RegisterFormState extends State<RegisterForm> {
 
   @override
   Widget build(BuildContext context) {
+    FocusNode _focusNodeEmail = new FocusNode();
+    FocusNode _focusNodePhoneNumber = new FocusNode();
+    FocusNode _focusNodePassword = new FocusNode();
+    FocusNode _focusNodeUsername = new FocusNode();
     TextEditingController _emailController = TextEditingController();
     TextEditingController _phoneNumberController = TextEditingController();
     TextEditingController _passwordController = TextEditingController();
     TextEditingController _usernameController = TextEditingController();
+
     return BaseView<RegisterModel>(
         builder: (context, model, child) => Form(
               key: _formKey,
@@ -31,38 +37,54 @@ class _RegisterFormState extends State<RegisterForm> {
                     color: Colors.white,
                     child: Column(
                       children: <Widget>[
-                        AppTextForm(
-                          textController: _usernameController,
-                          hintText: 'Username',
-                          iconData: Icons.person,
-                          errorMessage: 'Username should not be empty',
+                        EnsureVisibleWhenFocused(
+                          focusNode: _focusNodeUsername,
+                          child: AppTextForm(
+                            focusNode: _focusNodeUsername,
+                            textController: _usernameController,
+                            hintText: 'Username',
+                            iconData: Icons.person,
+                            errorMessage: 'Username should not be empty',
+                          ),
                         ),
                         Divider(
                           color: primaryColor,
                         ),
-                        AppTextForm(
-                          textController: _phoneNumberController,
-                          hintText: 'Phone Number',
-                          iconData: Icons.call,
-                          errorMessage: 'Phone Number should not be empty',
+                        EnsureVisibleWhenFocused(
+                          focusNode: _focusNodePhoneNumber,
+                          child: AppTextForm(
+                            textController: _phoneNumberController,
+                            focusNode: _focusNodePhoneNumber,
+                            hintText: 'Phone Number',
+                            iconData: Icons.call,
+                            errorMessage: 'Phone Number should not be empty',
+                          ),
                         ),
                         Divider(
                           color: primaryColor,
                         ),
-                        AppTextForm(
-                          textController: _passwordController,
-                          hintText: 'Password',
-                          iconData: Icons.lock,
-                          errorMessage: 'Password should not be empty',
+                        EnsureVisibleWhenFocused(
+                          focusNode: _focusNodePassword,
+                          child: AppTextForm(
+                            focusNode: _focusNodePassword,
+                            textController: _passwordController,
+                            hintText: 'Password',
+                            iconData: Icons.lock,
+                            errorMessage: 'Password should not be empty',
+                          ),
                         ),
                         Divider(
                           color: primaryColor,
                         ),
-                        AppTextForm(
-                          textController: _emailController,
-                          hintText: 'Email',
-                          iconData: Icons.email,
-                          errorMessage: 'Email should not be empty',
+                        EnsureVisibleWhenFocused(
+                          focusNode: _focusNodeEmail,
+                          child: AppTextForm(
+                            textController: _emailController,
+                            focusNode: _focusNodeEmail,
+                            hintText: 'Email',
+                            iconData: Icons.email,
+                            errorMessage: 'Email should not be empty',
+                          ),
                         ),
                       ],
                     ),
