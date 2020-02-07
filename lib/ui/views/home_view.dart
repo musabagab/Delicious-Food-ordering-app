4845769a3d0a7fa913b71c7dd482c8f00fafd43d
Musab Agab <musabagab2010@gmail.com>
2020-02-07 10:55:02 +0300

    refactored the drawer to  a widget

--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -12,46 +12,57 @@ class HomeView extends StatelessWidget {
         appBar: AppBar(
           title: Center(child: Text('Menu')),
         ),
-        drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              CircleImage(),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.restaurant_menu),
-                  title: Text('Menu'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.view_list),
-                  title: Text('My Orders'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.local_offer),
-                  title: Text('Offers'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.call_end),
-                  title: Text('Support'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.settings),
-                  title: Text('Settings'),
-                ),
-              ),
-            ],
-          ),
-        ),
+        drawer: new AppDrawer(),
         body: SafeArea(
           child: Text('HomeView'),
         ),
+      ),
+    );
+  }
+}
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          CircleImage(),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.restaurant_menu),
+              title: Text('Menu'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.view_list),
+              title: Text('My Orders'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.local_offer),
+              title: Text('Offers'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.call_end),
+              title: Text('Support'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ),
+        ],
       ),
     );
   }
