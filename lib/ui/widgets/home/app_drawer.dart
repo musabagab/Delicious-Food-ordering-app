64041a04a0ace56842767191aa2858d73d88b56c
Musab Agab <musabagab2010@gmail.com>
2020-02-07 10:56:19 +0300

    added the app drawer widget

---
+++ lib/ui/widgets/home/app_drawer.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          CircleImage(),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.restaurant_menu),
+              title: Text('Menu'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.view_list),
+              title: Text('My Orders'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.local_offer),
+              title: Text('Offers'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.call_end),
+              title: Text('Support'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
